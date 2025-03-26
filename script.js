b652ee19164baf19ee95dfcf46a9a2f2ac9d3380
mrjoshdude <mrjoshdude@me.com>
2025-03-26 11:01:41 +0000

    creating

---
+++ script.js
@@ -0,0 +1,21 @@
+function finishFunction() {
+    alert("Congratulations, you finished the quiz!");
+}
+
+function checkAnswers() {
+    let score = 0;
+    quiz = document.forms.Quiz.elements;
+    answer1 = quiz.bear.value;
+    if (answer1 == "black") {
+        score++;
+    }
+    answer2 = quiz.beets.value;
+    if (answer2 == "battlestar") {
+        score++;
+    }
+    answer3 = quiz.ducks.value;
+    if (answer3 == 20) {
+        score++;
+    }
+    alert(`Well done! Your score was ${score}`);
+}
